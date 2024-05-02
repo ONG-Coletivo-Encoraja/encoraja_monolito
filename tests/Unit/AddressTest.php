0fd556ee9eb81cec2d36119c340f4b47ad8a692a
andrelucasbaldaia <andrelucasbaldaia@gmail.com>
2024-05-01 22:48:03 -0300

    fix: address test changes made

--- tests/Unit/AddressTest.php
+++ tests/Unit/AddressTest.php
@@ -2,19 +2,63 @@
 
 namespace Tests\Unit;
 
+use App\Models\Address;
+use Database\Factories\AddressFactory;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+
 
 class AddressTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    use RefreshDatabase;
+    
+    public function test_creates_a_new_address(): void
+    {
+        $user = User::factory()->create();
+        
+        $addressAttributes = Address::factory()->make()->toArray();
+
+        $address = Address::factory()->create(['user_id' => $user->id] + $addressAttributes);
+        
+    
+        $this->assertInstanceOf(Address::class, $address);
+        $this->assertEquals($addressAttributes['street'], $address->street);
+        $this->assertEquals($addressAttributes['number'], $address->number);
+        $this->assertEquals($addressAttributes['neighbourhood'], $address->neighbourhood);
+        $this->assertEquals($addressAttributes['city'], $address->city);
+        $this->assertEquals($addressAttributes['zip_code'], $address->zip_code);
     }
+
+    public function test_address_update()
+    {
+        $user = User::factory()->create();
+        $address = Address::factory()->create(['user_id' => $user->id]);
+
+        $updatedData = [
+            'street' => 'Nova rua do endereço'
+        ];
+
+        $address->update($updatedData);
+
+        $updatedAddress = Address::find($address->id);
+
+        $this->assertEquals($updatedData['street'], $updatedAddress->street);
+    }
+
+    public function test_address_delete()
+    {
+        $user = User::factory()->create();
+        $address = Address::factory()->create(['user_id' => $user->id]);
+
+        $this->assertDatabaseHas('address', ['id' => $address->id]);
+
+        $address->delete();
+
+        $this->assertDatabaseMissing('address', ['id' => $address->id]);
+    }
+
 }
+
+
