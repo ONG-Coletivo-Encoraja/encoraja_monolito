55dcfe4525efccde221f5d711a18440b366a7f6e
andrelucasbaldaia <andrelucasbaldaia@gmail.com>
2024-06-23 18:15:44 -0300

    fix: resolving merge conflicts

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -30,7 +30,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('home.index', absolute: false));
     }
 
     /**
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -26,7 +26,6 @@
     @if (Route::has('login'))
         <div>
         @auth
-            <a href="{{ url('/dashboard') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">Área logada</a>
     @else
             <a href="{{ route('login') }}" class="auth-button">Log in</a>
 
