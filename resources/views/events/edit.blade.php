34028a00de5341a740538941f64cde6ddda45e11
andrelucasbaldaia <andrelucasbaldaia@gmail.com>
2024-06-25 20:41:26 -0300

    fix: added validation rules to the event editing form

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -11,19 +11,23 @@
 
         <div class="col-md-6">
             <label for="name" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="name" name="name" value="{{ $event->name }}" required>
+            <input type="text" class="form-control" id="name" name="name" value="{{ old('name', $event->name) }}" required>
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
         <div class="col-md-6">
             <label for="description" class="form-label">Descrição</label>
-            <input type="text" class="form-control" id="description" name="description" value="{{ $event->description}}" required>
+            <input type="text" class="form-control" id="description" name="description" value="{{ $event->description }}" required>
+            <x-input-error :messages="$errors->get('description')" class="mt-2" />
         </div>
         <div class="col-md-6">
             <label for="date" class="form-label">Data</label>
             <input type="date" class="form-control" id="date" name="date" value="{{ $event->date }}" required>
+            <x-input-error :messages="$errors->get('date')" class="mt-2" />
         </div>
         <div class="col-md-6">
             <label for="time" class="form-label">Time</label>
             <input type="time" class="form-control" id="time" name="time" value="{{ $event->time }}" required>
+            <x-input-error :messages="$errors->get('time')" class="mt-2" />
         </div>
         <div class="col-md-4">
             <label for="modality" class="form-label">Modalidade</label>
@@ -32,6 +36,7 @@
                 <option value="Hybrid" {{ $event->modality == 'Hybrid' ? 'selected' : '' }}>Híbrido</option>
                 <option value="Remote" {{ $event->modality == 'Remote' ? 'selected' : '' }}>Remoto</option>
             </select>
+            <x-input-error :messages="$errors->get('modality')" class="mt-2" />
         </div>
         <div class="col-md-4">
             <label for="status" class="form-label">Status</label>
@@ -40,6 +45,7 @@
                 <option value="Inactive" {{ $event->status == 'Inactive' ? 'selected' : '' }}>Inativo</option>
                 <option value="Pending" {{ $event->status == 'Pending' ? 'selected' : '' }}>Pendente</option>
             </select>
+            <x-input-error :messages="$errors->get('status')" class="mt-2" />
         </div>
         <div class="col-md-4">
             <label for="type" class="form-label">Tipo</label>
@@ -48,34 +54,42 @@
                 <option value="Workshop" {{ $event->type == 'Workshop' ? 'selected' : '' }}>Oficina</option>
                 <option value="Lecture" {{ $event->type == 'Lecture' ? 'selected' : '' }}>Palestra</option>
             </select>
+            <x-input-error :messages="$errors->get('type')" class="mt-2" />
         </div>
         <div class="col-md-6">
             <label for="target_audience" class="form-label">Público Alvo</label>
             <input type="text" class="form-control" id="target_audience" name="target_audience" value="{{ $event->target_audience }}" required>
+            <x-input-error :messages="$errors->get('target_audience')" class="mt-2" />
         </div>
         <div class="col-md-6">
             <label for="vacancies" class="form-label">Vagas</label>
             <input type="number" class="form-control" id="vacancies" name="vacancies" value="{{ $event->vacancies }}" required>
+            <x-input-error :messages="$errors->get('vacancies')" class="mt-2" />
         </div>
         <div class="col-md-6">
             <label for="social_vacancies" class="form-label">Vagas sociais</label>
             <input type="number" class="form-control" id="social_vacancies" name="social_vacancies" value="{{ $event->social_vacancies }}" required>
+            <x-input-error :messages="$errors->get('social_vacancies')" class="mt-2" />
         </div>
         <div class="col-md-6">
             <label for="regular_vacancies" class="form-label">Vagas normais</label>
             <input type="number" class="form-control" id="regular_vacancies" name="regular_vacancies" value="{{ $event->regular_vacancies }}" required>
+            <x-input-error :messages="$errors->get('regular_vacancies')" class="mt-2" />
         </div>
         <div class="col-md-6">
             <label for="material" class="form-label">Materiais necessários</label>
             <input type="text" class="form-control" id="material" name="material" value="{{ $event->material }}" required>
+            <x-input-error :messages="$errors->get('material')" class="mt-2" />
         </div>
         <div class="col-md-6">
             <label for="interest_area" class="form-label">Área de interesse</label>
             <input type="text" class="form-control" id="interest_area" name="interest_area" value="{{ $event->interest_area }}" required>
+            <x-input-error :messages="$errors->get('interest_area')" class="mt-2" />
         </div>
         <div class="col-md-6">
             <label for="price" class="form-label">Preço (opcional)</label>
             <input type="number" step="any" class="form-control" id="price" name="price" value="{{ $event->price }}" required>
+            <x-input-error :messages="$errors->get('price')" class="mt-2" />
         </div>
         <br>
         <div class="col-12">
