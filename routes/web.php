8ef569126f0f6bd939e779a7fa43a68267e6ae10
andrelucasbaldaia <andrelucasbaldaia@gmail.com>
2024-06-25 20:49:21 -0300

    fix: profile route deleted

--- routes/web.php
+++ routes/web.php
@@ -41,11 +41,4 @@ Route::get('/', function () {
     return view('homepage');
 })->name('homepage');
 
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__.'/auth.php';
